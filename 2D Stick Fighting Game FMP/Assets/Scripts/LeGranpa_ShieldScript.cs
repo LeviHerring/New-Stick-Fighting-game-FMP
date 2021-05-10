fda3c11a9c4ebf1305992a10118f3ddc4127e6c1
DESKTOP-FOBJSU5\theob <514121@chrome.escg.ac.uk>
2021-05-10 19:10:22 +0100

    new character, need to do the tags :0

---
+++ 2D Stick Fighting Game FMP/Assets/Scripts/LeGranpa_ShieldScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeGranpa_ShieldScript : MonoBehaviour
+{
+    public GameObject leGranpaShield;
+    private bool activeShield;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activeShield = false;
+        leGranpaShield.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Keypad3))
+        {
+            if (!activeShield)
+            {
+                leGranpaShield.SetActive(true);
+                activeShield = true;
+            }
+            else
+            {
+                leGranpaShield.SetActive(false);
+                activeShield = false;
+            }
+        }
+    }
+
+
+    public bool ActiveShield
+    {
+        get
+        {
+            return activeShield;
+        }
+        set
+        {
+            activeShield = value;
+        }
+    }
+}
